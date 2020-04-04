af0c67bf570459d34e6f2a7478267090f17129e4
Willy <simonandje@gmail.com>
2020-04-04 17:45:06 +0100

    envoie de fichiers du projet

---
+++ app/src/main/java/org/fgi/ttic5_fi2/app_signaux/MainActivity.java
@@ -0,0 +1,14 @@
+package org.fgi.ttic5_fi2.app_signaux;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
